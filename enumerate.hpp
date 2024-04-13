f7dc77de27ea219b4a5013620b8035497578e77b
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2024-04-13 16:15:38 +0900

    rename func to nullptr_func

--- enumerate.hpp
+++ enumerate.hpp
@@ -12,11 +12,11 @@ namespace enumerate {
 	consteval auto getEnumName() {
 #define toString(code) #code
 		const std::string_view nullptr_string(toString(nullptr));
-		const auto func = receieve_this_function_name<nullptr>();
-		const auto nullptr_pos = func.find_last_of(nullptr_string);
+		const auto nullptr_func = receieve_this_function_name<nullptr>();
+		const auto nullptr_pos = nullptr_func.find_last_of(nullptr_string);
 		auto target = receieve_this_function_name<Target>();
 		target.remove_prefix(nullptr_pos - nullptr_string.size() + 1);
-		target.remove_suffix(func.size() - nullptr_pos - 1);
+		target.remove_suffix(nullptr_func.size() - nullptr_pos - 1);
 		return target;
 #undef toString
 	}
