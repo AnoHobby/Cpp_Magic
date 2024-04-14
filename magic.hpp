af307fc354b8f9b764ae44c6ab4bea7a073d1ecc
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2024-04-14 23:00:43 +0900

    visit_tuple,make_array

--- magic.hpp
+++ magic.hpp
@@ -1,8 +1,13 @@
-#ifndef INCLUDE_GUARD_ENUMERATE_HPP
-#define INCLUDE_GUARD_ENUMERATE_HPP
+#ifndef INCLUDE_GUARD_MAGIC_HPP
+#define INCLUDE_GUARD_MAGIC_HPP
+
 import <string_view>;
 import <source_location>;
-namespace enumerate {
+import <tuple>;
+import <functional>;
+import <iterator>;
+import <array>;
+namespace magic {
 	template <auto Target>
 	consteval auto receieve_this_function_name() {
 		std::string_view function_name(std::source_location::current().function_name());
@@ -20,5 +25,28 @@ namespace enumerate {
 		return target;
 #undef toString
 	}
+	template <class F, class... T, std::size_t... I>
+	auto visit_tuple(std::tuple<T...>& tuple, const std::size_t& index, const F& function, std::index_sequence<I...>) {
+		std::function<void()> functions[] = {
+			([&] {function(std::get<I>(tuple)); })...
+		};
+		functions[index]();
+	}
+	template <class F, class... T>
+	auto visit_tuple(std::tuple<T...>& tuple, const std::size_t index, const F function) {
+		visit_tuple(tuple, index, function, std::make_index_sequence<sizeof...(T)>());
+	}
+	template <class F, class... T>
+	auto visit_tuple(std::tuple<T...>&& tuple, const std::size_t index, const F function) {
+		visit_tuple(tuple, index, function, std::make_index_sequence<sizeof...(T)>());
+	}
+	template <class T>
+	inline constexpr auto make_array(auto&&... args) {
+		return std::move(std::array<T, sizeof...(args)>{std::forward<decltype(args)>(args)...});
+	}
+	//inline constexpr auto auto_make_array(auto&&... args) {
+	//	return std::move(std::array<, sizeof...(args)>{std::forward<decltype(args)>(args)...});
+	//}
 };
+
 #endif
